9087ce7dbec6e1cd475d5e76f76df2b6d5c5bccf
David Op de Beeck <davidopdebeeck@hotmail.com>
2016-04-20 12:07:13 +0200

    Changes to Jenkins Dockerfile, plugin and workflow

--- ops/jenkins/flow.groovy
+++ ops/jenkins/flow.groovy
@@ -1,17 +1,13 @@
 
-/**
-* Networks
-*/
+// NETWORKS
 
-TEST_NETWORK = 'test-network';
-ACC_NETWORK = 'acc-network';
+NETWORK_TEST = 'test-network';
+NETWORK_ACC = 'acc-network';
 
-/**
-* Containers
-*/
+// CONTAINERS
 
-TASKBOARD_CONTAINER = 'taskboard-container';
-MYSQL_CONTAINER = 'mysql-container';
+CONT_TASKBOARD = 'taskboard-container';
+CONT_MYSQL = 'mysql-container';
 
 /**
 * Workflow
@@ -19,44 +15,70 @@ MYSQL_CONTAINER = 'mysql-container';
 
 node {
 
+    stage 'Git Clone'
+
     git 'https://github.com/DavidOpDeBeeck/taskboard.git'
 
-    testEnvironment()
+    stage 'Test Environment'
+
+    initTestEnvironment()
+    execTestEnvironment()
 
 }
 
-def testEnvironment() {
-  def taskboardContainer, mysqlContainer;
-
+def initTestEnvironment(){
   try {
-    createNetwork(TEST_NETWORK)
+    disconnect(taskboardContainer.id, NETWORK_TEST)
+    disconnect(mysqlContainer.id, NETWORK_TEST)
+    stop(taskboardContainer.id)
+    stop(mysqlContainer.id)
+    removeNetwork(NETWORK_TEST)
+  } catch (err) {}
+}
+
+def execTestEnvironment() {
+  def taskboardContainer, mysqlContainer;
+  try {
+    createNetwork(NETWORK_TEST)
 
     def currentDir = pwd()
 
-    sh 'cp ops/taskboard/back-end/test/Dockerfile Dockerfile'
-    def taskboardImage = docker.build(TASKBOARD_CONTAINER)
+    def taskboardImage = build(CONT_TASKBOARD, 'ops/taskboard/back-end/test/Dockerfile')
+    def mysqlImage = build(CONT_MYSQL, 'ops/taskboard/db/test/Dockerfile')
 
-    sh 'cp ops/taskboard/db/test/Dockerfile Dockerfile'
-    def mysqlImage = docker.build(MYSQL_CONTAINER)
+    def resultVolume = "${currentDir}/taskboard/rest-api/taskboard-domain/build/test-results:/app/taskboard-domain/build/test-results"
 
-    taskboardContainer = taskboardImage.run("-i -v ${currentDir}/taskboard/rest-api/taskboard-domain/build/test-results:/app/taskboard-domain/build/test-results")
+    taskboardContainer = taskboardImage.run("-i -v ${resultVolume}")
     mysqlContainer = mysqlImage.run("-i --name=mysql")
 
-    connect(taskboardContainer.id, TEST_NETWORK)
-    connect(mysqlContainer.id, TEST_NETWORK)
+    connect(taskboardContainer.id, NETWORK_TEST)
+    connect(mysqlContainer.id, NETWORK_TEST)
 
     exec(taskboardContainer.id, "gradle flywayMigrate -Denv=test")
     exec(taskboardContainer.id, "gradle repositoryTests -Denv=test")
-    step([$class: 'JUnitResultArchiver', testResults: "${currentDir}/taskboard/rest-api/taskboard-domain/build/test-results/TEST-*.xml"])
 
-  } catch (err) {}
+    step([$class: 'JUnitResultArchiver', testResults: "**/test-results/TEST-*.xml"])
+  } catch (err) {
+    throw err
+  }
     finally {
-       disconnect(taskboardContainer.id, TEST_NETWORK)
-       disconnect(mysqlContainer.id, TEST_NETWORK)
+       disconnect(taskboardContainer.id, NETWORK_TEST)
+       disconnect(mysqlContainer.id, NETWORK_TEST)
        stop(taskboardContainer.id)
        stop(mysqlContainer.id)
-       removeNetwork(TEST_NETWORK)
+       removeNetwork(NETWORK_TEST)
     }
+}
+
+/**
+* Docker functions
+*/
+
+def build(name, dockerfile) {
+  sh "cp ${dockerfile} Dockerfile"
+  def image = docker.build(name)
+  sh 'rm -f Dockerfile'
+  return image
 }
 
 def exec(container, cmd) {
@@ -82,27 +104,3 @@ def createNetwork(network) {
 def removeNetwork(network) {
 	try { sh "docker network rm ${network}" } catch(err) {}
 }
-
-/*
- Test Omgeving :
-
- - Maak container van REST API
- - Maak container van MYSQL (test)
-
- - Run MYSQL
- - Run REST API
-
- - Maak bridge netwerk aan en voeg beide toe
-
- - Run gradle tests en kijk naar output.
-
- -> TEST SUCCESSFULL
- -> TEST FAILED
-
- Acceptatie Omgeving :
-
-
- Productie (eindresultaat) :
-
-
-*/
