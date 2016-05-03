c2298379fe70c85fddc706c2dcc58e35dc13edd2
David Op de Beeck <davidopdebeeck@hotmail.com>
2016-05-03 16:44:32 +0200

    Fixed Dockerfile of web project

--- jenkins/flow.groovy
+++ jenkins/flow.groovy
@@ -18,28 +18,29 @@ def generateNetworkId() {
 //------------------------------
 
 def testDatabase = [
-    name      : env.BUILD_TAG + "-test-database",
-    port      : '3306',
+    name      : "$env.BUILD_TAG-test-database",
     user      : 'db-test',
     password  : 'g6qf98xy'
 ]
 
 def accDatabase = [
-    name      : env.BUILD_TAG + "-acc-database",
-    port      : '3306',
+    name      : "$env.BUILD_TAG-acc-database",
     user      : 'db-acc',
     password  : 'asiyat37'
 ]
 
 def web = [
-    name      : env.BUILD_TAG + "-web",
+    name      : "$env.BUILD_TAG-web",
     port      : '8000'
 ]
 
 def restApi = [
-    name      : env.BUILD_TAG + "-rest-api",
+    name      : "$env.BUILD_TAG-rest-api",
     port      : '9000'
 ]
+
+// the container id of the jenkns slave and the name of the network used in the environment
+def nodeId, network;
 
 //------------------------------
 // COMMIT STAGE
@@ -59,45 +60,61 @@ node('gradle')
 // TEST ENVIRONMENT STAGE
 //------------------------------
 
-def nodeId, testNetwork;
-
 stage 'REPOSITORY TESTS'
 
 node ('docker')
 {
-  nodeId = getNodeContainer()
-  testNetwork = generateNetworkId()
-  runContainer( testDatabase.name,
-                'mysql',
-                "-e MYSQL_DATABASE=taskboard -e MYSQL_USER=$testDatabase.user -e MYSQL_PASSWORD=$testDatabase.password -e MYSQL_ALLOW_EMPTY_PASSWORD=true" )
-  createNetwork(testNetwork)
-  connect(testNetwork, nodeId, 'docker')
-  connect(testNetwork, testDatabase.name, 'mysql')
+  // run database with test environment properties
+  runContainer( testDatabase.name, 'mysql', "-e MYSQL_DATABASE=taskboard -e MYSQL_USER=$testDatabase.user -e MYSQL_PASSWORD=$testDatabase.password -e MYSQL_ALLOW_EMPTY_PASSWORD=true" )
+  // generate a unique network id so we don't have collision
+  network = generateNetworkId()
+  // create a network (used to connect database and gradle slave)
+  createNetwork(network)
+  // connect the databe to the network with 'mysql' as his alias
+  connect(network, testDatabase.name, 'mysql')
 }
 
 node ('gradle')
 {
   try
   {
+    // get the container id of the gradle node
+    nodeId = getNodeContainer()
+    // connect the gradle slave to the network
+    node ('docker') { connect(network, nodeId, 'docker') }
+    // unstash the 'taskboard' files so we can execute gradle tasks
     unstash 'taskboard'
+    // we clean the project to secure new test results
+    sh "gradle clean"
+    // initialise the database with the specified tables
     sh "gradle flywayMigrate -Denv=test"
+    // execute the repository tests
     sh "gradle repositoryTests -Denv=test"
+    // archive the test results so we can use them at a later time
     step([$class: 'JUnitResultArchiver', testResults: "**/taskboard-domain/build/test-results/TEST-*.xml"])
   } catch (err)
   {
+    // when a gradle task fails we mark the stage as failed to be able to shutdown the dock containers
     stageFailed = true
   }
 }
 
 node ('docker')
 {
-  disconnect(testNetwork, nodeId)
-  disconnect(testNetwork, testDatabase.name)
-  removeNetwork(testNetwork)
-  removeContainer( testDatabase.name )
-  if (stageFailed)
+  // we disconnect the gradle slave and the test database from the network
+  disconnect(network,nodeId)
+  disconnect(network,testDatabase.name)
+  // we remove the network and the test database containers
+  removeNetwork(network)
+  removeContainer(testDatabase.name)
+  // if the stage has failed we throw an error so execution is stopped
+  if (stageFailed){
     error 'Stage failed'
+  }
 }
+
+// we ask the user if he wants to continue
+input message: 'Do you want to continue to acceptance?', ok: 'yes'
 
 //------------------------------
 // ACCEPTANCE ENVIRONMENT STAGE
@@ -107,83 +124,136 @@ stage "REST API TESTS"
 
 node ('docker')
 {
-  runContainer( accDatabase.name,
-                'mysql',
-                "-it -p $accDatabase.port:3306 -e MYSQL_DATABASE=taskboard -e MYSQL_USER=$accDatabase.user -e MYSQL_PASSWORD=$accDatabase.password -e MYSQL_ALLOW_EMPTY_PASSWORD=true" )
+  // run the database with the acceptance properties
+  runContainer(accDatabase.name,'mysql', "-d -e MYSQL_DATABASE=taskboard -e MYSQL_USER=$accDatabase.user -e MYSQL_PASSWORD=$accDatabase.password -e MYSQL_ALLOW_EMPTY_PASSWORD=true" )
+  // generate a unique network id so we don't have collision
+  network = generateNetworkId()
+  // create a network (used to connect database and gradle slave)
+  createNetwork(network)
+  // connect the database to the network with 'mysql' as his alias
+  connect(network, accDatabase.name, 'mysql')
 }
 
 node ('gradle')
 {
     try
     {
+      // get the container id of the gradle node
+      nodeId = getNodeContainer()
+      // connect the gradle slave to the network
+      node ('docker') { connect(network, nodeId, 'docker') }
+      // unstash the 'taskboard' files so we can execute gradle tasks
       unstash 'taskboard'
+      // we clean the project to secure new test results
+      sh "gradle clean"
+      // initialise the database with the specified acceptance tables
       sh "gradle flywayMigrate -Denv=acc"
+      // execute the rest api tests
       sh "gradle acceptanceTests -Denv=acc"
+      // archive the test results so we can use them at a later time
       step([$class: 'JUnitResultArchiver', testResults: "**/taskboard-rest-api/build/test-results/TEST-*.xml"])
     } catch (err)
     {
+      // when a gradle task fails we mark the stage as failed to be able to shutdown the dock containers
       stageFailed = true
     }
 }
 
 node ('docker')
 {
-    if (stageFailed) {
-      removeContainer( accDatabase.name )
-      error 'Stage failed'
-    }
+  // we disconnect the gradle slave and the acceptance database from the network
+  disconnect(network, nodeId)
+  disconnect(network, accDatabase.name)
+  // we remove the network container
+  removeNetwork(network)
+  // if the stage has failed we throw an error so execution is stopped
+  if (stageFailed){
+    error 'Stage failed'
+  }
 }
 
 stage "WEB TESTS"
 
 node ('gradle')
 {
+  // we unstash the 'taskboard' files to build the rest-api
   unstash 'taskboard'
+  // we ignore tests because the slave isn't connected to a database
   sh "gradle build -x test"
+  // we archive the rest api jar for later use
   archive '**/taskboard-rest-api/build/libs/*.jar'
-  dir ('conf') {
-    stash includes: 'acc.properties', name: 'config'
-  }
-  dir ('taskboard-rest-api') {
-    stash includes: '**', name: 'rest-api'
-  }
-  dir ('taskboard-web') {
-    stash includes: '**', name: 'web'
-  }
+  // we stash the acceptance properties to use with the rest api later on
+  dir ('conf') { stash includes: 'acc.properties', name: 'config' }
+  // we stash the rest api files for later use
+  dir ('taskboard-rest-api') { stash includes: '**', name: 'rest-api' }
+  // we stash the web files for later use
+  dir ('taskboard-web') { stash includes: '**', name: 'web' }
+}
+
+// network configuration for 'deploy'
+node ('docker')
+{
+  // generate a unique network id so we don't have collision
+  network = generateNetworkId()
+  // create a network (used to connect web, rest api, database and gradle slave)
+  createNetwork(network)
+  // connect the database to the network with 'mysql' as his alias
+  connect(network, accDatabase.name, 'mysql')
 }
 
 node ('docker')
 {
+  // we unstash the web project so we can build the docker images and run it
   unstash 'web'
+  // build the docker image (with the id of the build to make it unique)
   buildImage( web.name )
-  runContainer( web.name, web.name, "-p $web.port:8000" )
+  // run the image and expose the web port (8000)
+  runContainer( web.name, web.name, "-P" )
+  // connect the web frontend to the network
+  connect(network, web.name, 'web')
 }
 
-node ('docker') {
+node ('docker')
+{
+  // we unstash the config file and the rest api files to build the docker images and run it
   unstash 'config'
   unstash 'rest-api'
+  // build the docker image (with the id of the build to make it unique)
   buildImage( restApi.name )
-  runContainer( restApi.name, restApi.name, "-p $restApi.port:8080" )
+  // run the image and expose the web port (8080)
+  runContainer( restApi.name, restApi.name, "-P" )
+  // connect the rest api to the network
+  connect(network, restApi.name, 'rest-api')
 }
 
 node ('webdriver && gradle')
 {
   try
   {
+    // get the container id of the gradle node
+    nodeId = getNodeContainer()
+    // connect the gradle slave to the network
+    node ('docker') { connect(network, nodeId, 'docker') }
+    // unstash the 'taskboard' files
     unstash 'taskboard'
+    // we run the web tests
     sh "gradle webTests -Denv=acc"
   } catch (err)
   {
+    // when a gradle task fails we mark the stage as failed to be able to shutdown the dock containers
     stageFailed = true
   }
+  // we record the test result for later use
   step([$class: 'JUnitResultArchiver', testResults: "**/taskboard-web-test/build/test-results/TEST-*.xml"])
 }
 
 node ('docker')
 {
-  removeContainer( accDatabase.name )
-  if (stageFailed)
+  // removeContainer( accDatabase.name )
+  // if the stage has failed we throw an error so execution is stopped
+  if (stageFailed) {
     error 'Stage failed'
+  }
 }
 
 //------------------------------
