612e3758e1215169528c5b5f1fb6214de2648198
David Op de Beeck <davidopdebeeck@hotmail.com>
2016-05-17 23:47:07 +0200

    Re

--- taskboard/db/migration/V1_003__Add_task_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS task (
-  id          VARCHAR(255) NOT NULL,
-  title       VARCHAR(255) NOT NULL,
-  description VARCHAR(255) NOT NULL,
-  assignee    VARCHAR(255) NOT NULL,
-  PRIMARY KEY (id)
-);
